47472732c7c7a80b02cbe96137cfe52c19224254
SY <simssy2205@gmail.com>
2016-06-27 12:49:27 +0900

    add collision listener

--- Napxe.js
+++ Napxe.js
@@ -95,6 +95,7 @@ var Component = function() {
   this.direction = 0;
   this.speed = 0;
   this.real_speed = 0;
+  this.collisionListener = null;
 
   this.tag = "";
   this.count = 0;
@@ -102,6 +103,12 @@ var Component = function() {
 
 Component.prototype.setId = function(id) {
   this.id = id;
+
+  return this;
+};
+
+Component.prototype.setCollisionListener = function(listener) {
+  this.collisionListener = listener;
 
   return this;
 };
@@ -155,10 +162,11 @@ Component.prototype.update = function(tag) {
       this.real_speed = this.speed;
     }*/
     this.real_speed = this.speed;
-    this.speed *= 0.75;
+    this.speed *= 0.95;
     if(this.speed > 0) {
       var addy = Math.sin(this.direction) * this.real_speed * 0.1;
-      this.y += (addy > 0) ? 0 : addy;
+      if(addy < 0)
+        this.y += addy;
       this.x += Math.cos(this.direction) * this.real_speed * 0.1;
     }
 
@@ -188,19 +196,23 @@ Component.prototype.update = function(tag) {
         var bx = Math.floor(e.x);
         var by = Math.floor(e.y);
 
+        if(me.collisionListener != null) {
+          me.collisionListener(me, e, i);
+        }
+
         if(i == 2) {
           me.x = e.x - 1.001;
-          console.log("right wall");
+          //console.log("right wall");
         } else if (i == 1) {
           me.x = e.x + 1.001;
-          console.log("left wall");
+          //console.log("left wall");
         } else if(i == 0) {
           me.y = e.y - 1.001;
           me.count = 0;
-          console.log("floor");
+          //console.log("floor");
         } else if(i == 3) {
           me.y = e.y + 1.001;
-          console.log("roof");
+          //console.log("roof");
         }
       }
     });
@@ -328,3 +340,91 @@ var Util = {
       return 0;
   }
 };
+
+window.onload = function() {
+  var material = new Material();
+  material.setTexture("#7D7D7D");
+  material.setBounce(1);
+  material.setSolid(true);
+
+  var e = new Environment();
+  e.useGravity = true;
+  e.gravityDirection = 270;
+  e.gravityValue = 0.98;
+  e.useFriction = true;
+  e.frictionValue = 0.5;
+
+  var we = new Environment();
+  we.useGravity = true;
+  we.gravityDirection = 270;
+  we.gravityValue = 0.098;
+
+  var gm = new Material();
+  gm.setTexture("#4d4d4d");
+  gm.setSolid(true);
+
+  var wm = new Material();
+  wm.setTexture("#03a7fa");
+  wm.setSolid(true);
+
+  var P = new Component();
+  P.setMaterial(material);
+  P.setEnvironment(e);
+  P.setId("Player");
+  P.setWidth(1).setHeight(1);
+  P.isMove = true;
+
+  P.setCollisionListener(function(me, other, side) {
+    if(side == 0) {
+      me.setVel(90, 4);
+    }
+  });
+
+  var G = new Component();
+  G.setMaterial(gm);
+  G.setWidth(1).setHeight(1);
+
+  var W = new Component();
+  W.setMaterial(wm);
+  W.setEnvironment(we);
+  W.setWidth(1).setHeight(1);
+  W.isMove = true;
+
+  var camera = new Camera();
+  camera.setViewPos(P);
+  camera.setViewSize(new Vec2(16, 16));
+
+  var map = [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  ];
+
+  var data = [
+    Object.create(W),
+    Object.create(G),
+    Object.create(P),
+  ];
+
+  Napxe.link(camera);
+  Napxe.setCanvas(document.getElementById("canvas"));
+  Napxe.loadMap(map);
+  Napxe.mapLinker(data);
+  Napxe.run();
+
+  window.onkeydown = function(e) {
+    var key = e.keyCode;
+    if(key == 65) {
+      Napxe.get("Player").setVel(180, 3);
+    } else if(key == 68) {
+      Napxe.get("Player").setVel(0, 3);
+    } else if(key == 87) {
+      Napxe.get("Player").setVel(90, 4);
+    }
+  }
+}
